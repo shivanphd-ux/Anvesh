22a9077c528a3911bcc6c96785c19003b2196b1e
shivanphd-ux <shivanphd@gmail.com>
2025-09-12 11:34:14 +0530

    Update prod.cpp

--- prod.cpp
+++ prod.cpp
@@ -6,5 +6,7 @@ int main()
 	scanf("%d%d",&a,&b);
 	c=a*b;
 	printf("Product of %d and %d = %d",a,b,c);
+	int d=a+b+c;
+	printf("\nSummation of all %d,%d,%d = %d",a,b,c,d)
 	printf("\nThank you...");
 }
