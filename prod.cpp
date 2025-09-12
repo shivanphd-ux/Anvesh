0c1af6d9a77b4e0953a81ed0bdc6d861bbace724
YourGitHubUsername <your_email@example.com>
2025-09-12 11:32:35 +0530

    Added prod.cpp file

---
+++ prod.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter a and b values: ");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("Product of %d and %d = %d",a,b,c);
+	printf("\nThank you...");
+}
