7acbe1e010ea609dde30592c0f7ef814566cbc24
shivanphd-ux <shivanphd@gmail.com>
2025-09-13 14:59:06 +0530

    Added general.cpp

---
+++ general.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter a and b values: ");
+	scanf("%d%d",&a,&b);
+	c=a*b;
+	printf("Product of %d and %d = %d",a,b,c);
+	int d=a+b+c;
+	printf("\nSummation of all %d,%d,%d = %d",a,b,c,d)
+	printf("\nThank you...");
+}
